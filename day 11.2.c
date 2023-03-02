178dffd1132ee1756041624216269a1ee0fb04a4
dhruvdclanguage <126230133+dhruvdclanguage@users.noreply.github.com>
2023-03-02 12:45:37 +0530

    Add files via upload

---
+++ day 11.2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main (){
+	
+	int a ;
+	
+	printf("Enter any Number to find it is Odd or Even  : ");
+	scanf("%d",&a);
+	
+	a%2==0
+	?
+		printf("\n\ncongras!!\nValue you have Enter is Even.")
+	:
+		printf("\n\ncongras!!\nValue you have Enter is Odd.");	
+	
+	return 0 ;
+}
